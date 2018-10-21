450923b0e0ad0508ca4fc3b1c833d9580865f560
Zach Chen <szmuchen@gmail.com>
2018-10-21 10:18:13 -0400

    fixed some grammar in the scenarios

--- mad_libs.py
+++ mad_libs.py
@@ -3,25 +3,25 @@ def beach():
   noun = input("Enter a noun(name):").title()
   verb = input("Enter a verb(past):")
   adjective = input("Enter a adjective:")
-  print(noun + " went to the beach one day and " + verb + " in the ocean, when " + noun + " was done he/she felt " + adjective +", " + noun + " had a fun time at the beach. ")
+  print(noun + " went to the beach one day and " + verb + " in the ocean. When " + noun + " was done he/she felt " + adjective +", " + noun + " had a fun time at the beach. ")
 
 def school():
   print("\nWelecome to the school theme Mad Libs")
   noun = input("Enter a noun(name):").title()
   verb = input("Enter a verb(past):")
   adjective = input("Enter a adjective:")
-  print("On a cloudy Monday morning right before school " + noun + " " + verb + " fruit loops then went on the school bus, at school " + noun + " looked " + adjective + ".")
+  print("On a cloudy Monday morning right before school, " + noun + " " + verb + " fruit loops then went on the school bus. At school " + noun + " looked " + adjective + ".")
 
 def restaurant():
   print("\nWelcome to the restaurant themed Mad Libs")
   noun = input("Enter a noun(name):").title()
   verb = input("Enter a verb(past):")
   adjective = input("Enter a adjective:")
-  print("At the most famous restaurant in town " + noun + " " + verb + " his favorite meal, but after he/she felt " + adjective +".")
+  print("At the most famous restaurant in town, " + noun + " " + verb + " his favorite meal, but after he/she felt " + adjective +".")
   
 def main():
   print("Hello welcome to the game Mad Libs.")
-  choice = input("Which Mad Lib theme do you want to complete\n1.Beach\n2.School\n3.Restaurant\n>>>").lower()
+  choice = input("Which Mad Libs theme do you want to complete\n1.Beach\n2.School\n3.Restaurant\n>>>").lower()
 
   if choice == "beach":
     beach()
